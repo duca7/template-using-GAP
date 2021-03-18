6166c5d0776e5ff3ef496f6ab090dbd125da3cb7
duc <hohuynhduca7bh123@gmail.com>
2021-03-18 22:03:26 +0700

    done scroll

---
+++ scroll-animations/src/js/main.js
@@ -0,0 +1,11 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".section").forEach(section => {
+    ScrollTrigger.create({
+        trigger: section,
+        start: 'top top',
+        pin: true,
+        pinSpacing: false,
+    })
+
+});
